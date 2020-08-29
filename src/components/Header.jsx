71449cd10d90800baa5b08c85da6fead0f75470b
BreDi <40304887+Glendi123@users.noreply.github.com>
2020-08-29 17:13:35 +0300

    main

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../img/google.png'
+
+const Header = () => {
+    return <header className='header'>
+        <img src={logo} alt="Логотип" />
+    </header>
+}
+
+export default Header;
